b9cc355190469b986883439ec423c1962ed87279
denizabva <13569@holbertonstudents.com>
2026-05-02 18:30:05 +0400

    Create 11-the_western_exchange.py

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Function that transposes a numpy.ndarray.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Returns the transpose of a numpy array.
+
+    Args:
+        matrix (numpy.ndarray): input array
+
+    Returns:
+        numpy.ndarray: transposed array
+    """
+    return matrix.T
