39c898670656f57159f50e336565c6a147861add
denizabva <13569@holbertonstudents.com>
2026-05-02 17:18:49 +0400

    Create 2-size_me_please.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the shape of a matrix.
+"""
+
+
+def matrix_shape(matrix):
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
