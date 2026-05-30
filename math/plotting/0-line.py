0e670c4b95afd03d76f311877bbd988cca937924
denizabva <13569@holbertonstudents.com>
2026-05-30 22:25:12 +0400

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+"""Module that plots a cubic line graph"""
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """Function that plots y = x^3 as a red line"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     x = np.arange(0, 11)
-    plt.plot(x, y, 'r-')   # solid red line
-    plt.xlim(0, 10)        # x-axis range
+    plt.plot(x, y, 'r-')
+    plt.xlim(0, 10)
