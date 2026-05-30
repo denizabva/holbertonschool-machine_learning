a6c6e79b01434452db6f0e07124468cbfd679b14
denizabva <13569@holbertonstudents.com>
2026-05-30 22:38:39 +0400

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -9,14 +9,19 @@ def frequency():
     """Function that plots the distribution of student grades"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-
+    
+    # Keep the original figure size from the starter template
+    plt.figure(figsize=(6.4, 4.8))
+    
+    # Plot the histogram with 10-unit bins and black borders
     plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
-
+    
+    # Labels and Titles
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
+    
+    # Set explicit X-axis boundaries to match the reference
+    plt.xlim(0, 100)
 
-    plt.show()
-
-
-frequency()
+# DO NOT call frequency() or plt.show() here
