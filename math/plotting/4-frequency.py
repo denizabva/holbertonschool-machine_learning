271f4350f18ce649028f2edd51da359834fffb3a
denizabva <13569@holbertonstudents.com>
2026-05-30 22:32:04 +0400

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,7 +13,7 @@ def frequency():
 
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+    plt.hist(student_grades, bins=10, edgecolor='black')
 
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
