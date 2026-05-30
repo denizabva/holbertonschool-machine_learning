a7498e6587c1fc78dfdefa80f4a7379294c939ce
denizabva <13569@holbertonstudents.com>
2026-05-30 15:31:28 +0400

    Add exponential PDF method

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -17,3 +17,10 @@ class Exponential:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(1 / (sum(data) / len(data)))
+
+    def pdf(self, x):
+        """Calculates the PDF for a given time period."""
+        if x < 0:
+            return 0
+
+        return self.lambtha * (2.7182818285 ** (-self.lambtha * x))
