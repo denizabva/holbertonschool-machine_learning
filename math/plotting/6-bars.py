8054e2b75d7fe57c660d2354b185813fded079f5
denizabva <13569@holbertonstudents.com>
2026-05-30 22:49:52 +0400

    Update 6-bars.py

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,34 +2,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(1)
+grades = np.random.randint(0, 100, 50)
 
-def bars():
-    np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4, 3))
-    plt.figure(figsize=(6.4, 4.8))
+bins = range(0, 101, 10)
 
-    labels = ["Farrah", "Fred", "Felicia"]
-    x = np.arange(len(labels))
-    width = 0.5
+plt.hist(
+    grades,
+    bins=bins,
+    edgecolor='black'
+)
 
-    apples = fruit[0]
-    bananas = fruit[1]
-    oranges = fruit[2]
-    peaches = fruit[3]
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.title("Project A")
 
-    plt.bar(x, apples, width, color='red', label='apples')
-    plt.bar(x, bananas, width, bottom=apples, color='yellow', label='bananas')
-    plt.bar(x, oranges, width, bottom=apples+bananas, color='#ff8000', label='oranges')
-    plt.bar(x, peaches, width, bottom=apples+bananas+oranges, color='#ffe5b4', label='peaches')
-
-    plt.xticks(x, labels)
-
-    plt.ylabel("Quantity of Fruit")
-    plt.title("Number of Fruit per Person")
-
-    plt.ylim(0, 80)
-    plt.yticks(np.arange(0, 81, 10))
-
-    plt.legend()
-
-    plt.show()
+plt.show()
