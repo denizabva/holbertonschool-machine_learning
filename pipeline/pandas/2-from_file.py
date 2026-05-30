7d8aa6e6387131e6b1b2a86ef546f7a407646f2f
denizabva <13569@holbertonstudents.com>
2026-05-30 20:36:37 +0400

    Create 2-from_file.py

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+def from_file(filename, delimiter):
+    """
+    Loads data from a file into a pandas DataFrame.
+
+    Args:
+        filename (str): file to load from
+        delimiter (str): column separator
+
+    Returns:
+        pd.DataFrame: loaded dataframe
+    """
+    return pd.read_csv(filename, delimiter=delimiter)
